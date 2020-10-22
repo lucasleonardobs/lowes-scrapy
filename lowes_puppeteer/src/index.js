79b96c8d0c41b113386ce0ebc55bbff8a7cf942c
lucasleonardobs <llbs@cin.ufpe.br>
2020-10-22 02:47:20 -0300

    Create price scraping from csv.

---
+++ lowes_puppeteer/src/index.js
@@ -0,0 +1,69 @@
+const puppeteer = require('puppeteer-extra');
+const AdblockerPlugin = require('puppeteer-extra-plugin-adblocker');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+
+const csv = require('csv-parser');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const fs = require('fs');
+const path = require('path');
+
+const refrigerators_path = path.join(__dirname, '../../data/refrigerators.csv');
+const prices_path = path.join(__dirname, '../../data/prices.csv');
+
+puppeteer.use(AdblockerPlugin());
+puppeteer.use(StealthPlugin());
+
+const scrapingPrice = async (links) => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+
+  await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36');
+
+  prices = [];
+
+  for(link of links) {
+    await page.goto(link);
+    const check = page.$('span.aPrice.large span');
+
+    if (!check) {
+      prices.push('[Error] - This Page is Missing or Moved');
+      continue;
+    }
+
+    await page.waitForSelector('span.aPrice.large span');
+
+    const price = await page.$eval('span.aPrice.large span', price => {
+      return price.innerText
+    });
+
+    console.log(price);
+    prices.push(price);
+  }
+  console.log(prices);
+  browser.close();
+  return prices;
+}
+
+const fetchCsvLinks = () => {
+  const links = []
+
+  fs.createReadStream(refrigerators_path)
+    .pipe(csv({}))
+    .on('data', row => { links.push(row.URL) })
+    .on('end', async () => { 
+      const prices = await scrapingPrice(links);
+
+      const csvWriter = createCsvWriter({
+        path: prices_path,
+        header: [
+          { id: 'price', title: 'Price' },
+        ]
+      })
+
+      csvWriter.writeRecords(prices).then(() => console.log('The CSV file was written successfully'))
+    })
+
+  return links
+}
+
+fetchCsvLinks();
